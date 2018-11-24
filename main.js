d2a0d850b11c10f7558bba7a9f3b45047702012a
biscuitmanz <doddsanater87@gmail.com>
2018-11-24 21:55:38 +0000

    done most of front page

---
+++ main.js
@@ -0,0 +1,23 @@
+$(document).ready(()=>{
+let contact = $('#cBtn');
+    $(contact).hover(()=>{
+    
+        $(contact).animate({
+            left:-300,
+            top:-300,
+            width:400,
+            height:600,
+            backgroundColor:'#fff'
+        }, 500)
+
+        // $(contact).append(<input></input>)
+        
+    })
+
+
+
+
+
+
+
+})
