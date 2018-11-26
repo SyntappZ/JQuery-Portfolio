368e8480e3e48ac43c39a6b130af291a8d3b6087
biscuitmanz <doddsanater87@gmail.com>
2018-11-26 23:34:24 +0000

    done about page

--- main.js
+++ main.js
@@ -27,19 +27,72 @@ let homeBtn = $('#homeBtn');
 let aboutBtn = $('#aboutBtn');
     proBtn.click(()=>{
         pageBorder(proBtn, homeBtn, aboutBtn)
-        $('#imgpage').css('transform', 'scale(1)')
-            $('.frontWrap').fadeOut(600,()=>{
-                $('#imgpage').animate({left:2000})
-            })
-            
+        imgScale('scale(1)', 0); 
+        frontFadeOut(0);
+        imgpageSlide(2000, 600);
+        coverSlide(-1000, 1100);
+        projectEdge(0, 2300);
+        
     })
-function pageBorder(btn1, btn2, btn3){
+    homeBtn.click(()=>{
+        pageBorder(homeBtn, proBtn, aboutBtn)
+        
+    })
+    aboutBtn.click(()=>{
+        pageBorder(aboutBtn, proBtn, homeBtn)
+        imgScale('scale(1)', 0); 
+        frontFadeOut(0);
+        imgpageSlide(2000, 600);
+        setTimeout(aboutInfo.bind(null, 'scale(1)', '1'), 1400)
+        aboutBar(40, 1600)
+    })
+
+
+    
+            
+       
+    function imgScale(scale){
+        $('#imgpage').css('transform', scale);
+    }
+    function frontFadeOut(fODel){
+        $('.frontWrap').delay(fODel).fadeOut(600);
+    }
+    function frontFadeIn(fIDel){
+        $('.frontWrap').delay(fIDel).fadeIn(600);
+    }
+    function imgpageSlide(dir, iSDel){
+        $('#imgpage').delay(iSDel).animate({left:dir})
+    }
+    function coverSlide(x, cSDel){
+        $('#cover').delay(cSDel).animate({top:x})
+    }
+    function projectEdge(edge, eDel){
+        $('#leftEdge').delay(eDel).animate({left:edge},300);
+        $('#rightEdge').delay(eDel).animate({right:edge},300);
+    }
+
+
+    function pageBorder(btn1, btn2, btn3){
         btn1.css('border-bottom', 'solid 4px #FF8F00')
         btn2.css('border-bottom', 'none')
         btn3.css('border-bottom', 'none')
 }
 
+      
 
+    //about
+
+    function aboutInfo(mIscale, op){
+        $('.myInfo').css({
+            'transform': mIscale,
+            'opacity': op
+        });
+    }
+
+    function aboutBar(bot, aBDel){
+        $('.infoBar').delay(aBDel).animate({bottom: bot})
+    }
+    
 
 
 
