16bb08d92c782f22714abe56fdcf9032fa9781e9
biscuitmanz <doddsanater87@gmail.com>
2018-12-01 17:58:00 +0000

    fixed project selector

--- main.js
+++ main.js
@@ -12,22 +12,50 @@ let contact = $('#cBtn');
         }, 400,).css({
             'background-color': 'transparent',
             'border': 'solid #FF8F00 1px',
+            'border-radius':'5px',
             'color': '#FFf',
             'cursor':'default'
             
         });
+        $('#com').css('display', 'none')
         $('.hoverContact').css({
             'display': 'block',
         })
     })
 
+
+    function contactShrink(){
+        contact.animate({
+            left:0,
+            top:0,
+            width:150,
+            height:150,
+        },400, ()=>{
+            contact.css({
+                'background-color': '#FF8F00',
+                'border': 'none',
+                'border-radius':'10px',
+                'color': '#000',
+                'cursor':'pointer'
+            });
+        })
+        $('.far fa-comment-alt').css('display', 'block')
+        $('.hoverContact').css({
+            'display': 'none',
+        })
+    }
+
+let i = 1;
 let boxS = '2px 2px 10px 5px rgba(0, 0, 0, 0.137)';
 let proBtn = $('#projectBtn');
 let homeBtn = $('#homeBtn');
 let aboutBtn = $('#aboutBtn');
+
+
     proBtn.click(()=>{
         if( $('#imgpage').offset().left === 2000){//from about
             pageBorder(proBtn, homeBtn, aboutBtn);
+            
             arrowLinks('0', 'scale(0.8)')
             aboutBar(-1100, 500);
             setTimeout(aboutInfo.bind(null, 'scale(0.7)', '0'), 900);
@@ -39,6 +67,8 @@ let aboutBtn = $('#aboutBtn');
             setTimeout(cred.bind(null, '1'), 3400);
         }else{//from home
             pageBorder(proBtn, homeBtn, aboutBtn);
+            
+            contactShrink()
             imgScale('scale(1)', 0); 
             frontFadeOut(0);
             imgpageSlide(2000, 600);
@@ -47,6 +77,7 @@ let aboutBtn = $('#aboutBtn');
             setTimeout(projectWrap.bind(null, '1', 'scale(1)'), 2700);
             nextButton(3000, 110);
             setTimeout(cred.bind(null, '1'), 3300);
+            
         }
       
         
@@ -89,6 +120,7 @@ let aboutBtn = $('#aboutBtn');
             setTimeout(arrowLinks.bind(null, '1', 'scale(1)'), 3500)
         }else{//from home 
             pageBorder(aboutBtn, proBtn, homeBtn)
+            contactShrink()
             imgScale('scale(1)', 0); 
             frontFadeOut(0);
             imgpageSlide(2000, 600);
@@ -171,20 +203,40 @@ let aboutBtn = $('#aboutBtn');
         $('#cred').css('opacity', cOp);
     }
    
-    let dailyG =  $('.image').attr("src", "img/pro1.png");
-    let rapidJs = $('.image').attr("src", "img/pro2.png");
-    let sClothes = $('.image').attr("src", "img/pro3.png");
+    let picArr = [
+        "img/pro1.png",
+        "img/pro2.png",
+        "img/pro3.png"
+    ]
+   
    
     $('#nextBtn').click(()=>{
-       if($('.image').attr() === dailyG){
-        rapidJs
-       }
-       
-        
+       projectChange(picArr[i]);
+       i++
+        if(i === picArr.length){
+            i = 0;
+        }
     })
+    function projectChange(x){
+        $('.proImg').css({
+            transform: 'scale(0.8)',
+            opacity: '0'
+        })
+        setTimeout(changeImg.bind(null, x),500);
+        setTimeout(fadeDelay, 800);
 
-    
-       
+    }
+
+    function changeImg(img){
+        $('.image').attr("src", img);
+    }
+        
+    function fadeDelay(){
+        $('.proImg').css({
+            transform: 'scale(1)',
+            opacity: '1'
+        })
+}
 
 
 
