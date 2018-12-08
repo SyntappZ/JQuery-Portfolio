1748949468b2aaaacc24a4823a263e25b272fa74
biscuitmanz <doddsanater87@gmail.com>
2018-12-08 20:57:29 +0000

    finished project information

--- main.js
+++ main.js
@@ -258,7 +258,12 @@ let timing = [
         if(i === picArr.length){
             i = 0;
         }
+        
+        edgeInfo(null, '0')
+        setTimeout(removePara, 400);
     })
+
+
     function projectChange(x){
         $('.proImg').css({
             transform: 'scale(0.8)',
@@ -290,6 +295,7 @@ let timing = [
      $('#info').click(()=>{
         proInfo(300);
         insideWidth(270);
+        project();
     })
 
 
@@ -302,6 +308,7 @@ let timing = [
     $('#git').click(()=>{
         project();
      })
+     
 
      function insideWidth(a){
          $('.insideEdge').animate({
@@ -316,20 +323,32 @@ let timing = [
     }
 
 
+    let dailyPro = '<p>Daily Guide is a personal project im making just to make the daily choices easier its made with html/css and vanilla javascript. this is the first site that i done javascript on my own and not with help from tutorials, i now know how to use random in javascript, also it really helped me understand arrays more i learnt how to use choose what information is showed from the array depending on the day of the week, i also learnt how to make a slide out nav and a page loader while making this project. this page is fully responsive tablet and phone it helped me understand how to change the functionality of the page for the different page sizes.</p>'
+    let rapid = '<p>Rapid Javascript was my first ever javascript project it was a follow along tutorial on youtube and then turned it into my own game of javascript words that get harder as you progress and i added changes like a button to it to change the difficulty and start and end screens. buildiing this project helped me understand how to use countdowns and how if statements work also i understand arrays a bit better now, i also know how to grab dom elements with javascript now too.</p>'
+    let sClothes = '<p>Syntappz Clothes was my first clothes site i learnt quite a lot about javascript making this site this is the most javascript i have wrote on my own before my jquery portfolio, i would say this was my first proper site, i learnt how to use a loop to make the same animation happen to multiple objects which saved some code writing and this has made me understand loops a bit more, i learnt a lot about animations making this site and using setTimeout to make them run at diffent times, i also understand how to use parameters a lot better in functions now and using images in javascript too. i have not made this site responsive as this was made just to learn javascript, maybe i will look into that in the future.</p>'
     function project(){
         if(picArr[i] === picArr[1]){
             $('#visit a').attr('href', 'https://daily-guide.tk/');
             $('#git a').attr('href', 'https://github.com/SyntappZ/Daily-Guide');
+           setTimeout(edgeInfo.bind(null, dailyPro, '1'), 400);
         }
         else if(picArr[i] === picArr[2]){
             $('#visit a').attr('href', 'https://rapid-javascript.tk/');
             $('#git a').attr('href', 'https://github.com/SyntappZ/Rapid-Javascript');
+            setTimeout(edgeInfo.bind(null, rapid, '1'), 400);
         }
         else{
             $('#visit a').attr('href', 'https://syntappz-clothes.tk/');
             $('#git a').attr('href', 'https://github.com/SyntappZ/syntappz-clothes');
+            setTimeout(edgeInfo.bind(null, sClothes, '1'), 400);
         }
     }
 
-
+    function edgeInfo(app, eOp){
+        $('#eInfo').append(app);
+        $('.edgeInfo').css('opacity', eOp);
+    }
+    function removePara(){
+        $('#eInfo p').remove();
+    }
 })
