42b47a0a7c6757e9d06cc6cd60a5993aaf6620ac
BiscuitmanZ <doddsanater87@gmail.com>
2018-12-16 20:15:51 +0000

    reworked the info box for the projects

--- main.js
+++ main.js
@@ -110,11 +110,7 @@ let timing = [
             inprogress = true;
             if($('#cover').offset().top === -1000){   //from projects
                 pageBorder(homeBtn, proBtn, aboutBtn);
-                    if( $('.edge').width() === 300){
-                        projectEdge(-300);
-                    }else{
-                        projectEdge(-40);
-                    }
+                projectEdge(-40);
                 setTimeout(cred.bind(null, -40), 0);
                 nextButton(-50, 0);
                 setTimeout(projectWrap.bind(null, '0', 'scale(0.8)'), timing[0]);
@@ -151,12 +147,7 @@ let timing = [
             inprogress = true;
             if($('#cover').offset().top === -1000){
                 pageBorder(aboutBtn, proBtn, homeBtn);
-                
-                    if( $('.edge').width() === 300){
-                        projectEdge(-300);
-                    }else{
-                        projectEdge(-40);
-                    }
+                projectEdge(-40);
                 setTimeout(cred.bind(null, -40), 0);
                 nextButton(-50, 0);
                 setTimeout(projectWrap.bind(null, '0', 'scale(0.8)'), timing[0]);
@@ -277,9 +268,8 @@ let timing = [
         if(i === picArr.length){
             i = 0;
         }
+        closeInfo();
         
-        edgeInfo(null, '0')
-        setTimeout(removePara, 400);
     })
 
 
@@ -312,9 +302,15 @@ let timing = [
 
      //info button
      $('#info').click(()=>{
-        proInfo(300);
-        insideWidth(270);
-        infoChanger();
+         if( $('.infobox').css('opacity' === '1')){
+            infoChanger();
+         }else{
+            infoBox('1', '100px');
+            setTimeout(() => {
+                infoChanger();
+            }, 400);
+         }
+        
         
     })
 
@@ -330,24 +326,12 @@ let timing = [
      })
      
 
-     function insideWidth(a){
-         $('.insideEdge').animate({
-             width:a
-         })
-     }
-
-    function proInfo(w){
-        $('.edge').animate({
-            width:w
-        },timing[0]);
-    }
-    function edgeClose(){
-        proInfo(0);
-        insideWidth(0);
-    }
+     
+    
+   
 
 //project info changer
-    let dailyPro = '<p>Daily Guide is a personal project im making just to make the daily choices easier its made with html/css and vanilla javascript. this is the first site that i done javascript on my own and not with help from tutorials, i now know how to use random in javascript, also it really helped me understand arrays more i learnt how to use choose what information is showed from the array depending on the day of the week, i also learnt how to make a slide out nav and a page loader while making this project. this page is fully responsive tablet and phone it helped me understand how to change the functionality of the page for the different page sizes.</p>'
+    let dailyPro = '<p>Daily Guide is a personal project i made just to make the daily choices easier its made with html/css and vanilla javascript. this is the first site that i done javascript on my own and not with help from tutorials, i now know how to use random in javascript, also it really helped me understand arrays more i learnt how to use choose what information is showed from the array depending on the day of the week, i also learnt how to make a slide out nav and a page loader while making this project. this page is fully responsive tablet and phone it helped me understand how to change the functionality of the page for the different page sizes.</p>'
     let rapid = '<p>Rapid Javascript was my first ever javascript project it was a follow along tutorial on youtube and then turned it into my own game of javascript words that get harder as you progress and i added changes like a button to it to change the difficulty and start and end screens. buildiing this project helped me understand how to use countdowns and how if statements work also i understand arrays a bit better now, i also know how to grab dom elements with javascript now too.</p>'
     let sClothes = '<p>Syntappz Clothes was my first clothes site i learnt quite a lot about javascript making this site this is the most javascript i have wrote on my own before my jquery portfolio, i would say this was my first proper site, i learnt how to use a loop to make the same animation happen to multiple objects which saved some code writing and this has made me understand loops a bit more, i learnt a lot about animations making this site and using setTimeout to make them run at diffent times, i also understand how to use parameters a lot better in functions now and using images in javascript too. i have not made this site responsive as this was made just to learn javascript, maybe i will look into that in the future.</p>'
     function project(){
@@ -367,22 +351,52 @@ let timing = [
            
         }
     }
-    function infoChanger(){
-        if(picArr[i] === picArr[1]){
-            setTimeout(edgeInfo.bind(null, dailyPro, '1'), 400);
-        }
-        else if(picArr[i] === picArr[2]){
-            setTimeout(edgeInfo.bind(null, rapid, '1'), 400);
+
+    function pInfo(app){
+        if($('.pInfo').css('opacity') === '1'){
+         null
         }else{
-            setTimeout(edgeInfo.bind(null, sClothes, '1'), 400);
+            $('.pInfo').append(app).css({
+                opacity: '1',
+                transform: 'scale(1)'
+            })
+           
         }
+       
     }
 
-    function edgeInfo(app, eOp){
-        $('#eInfo').append(app);
-        $('.edgeInfo').css('opacity', eOp);
+    function closeInfo(){
+        $('.pInfo').css({
+            opacity: '0',
+            transform: 'scale(0.6)'
+        })
+         setTimeout(() => {
+             $('.pInfo p').remove();
+         }, 400);
+      }
+        
+    
+
+    function infoChanger(){
+        if(picArr[i] === picArr[1]){
+            pInfo(dailyPro);
+        }
+        else if(picArr[i] === picArr[2]){
+            pInfo(rapid);
+        }else{
+            pInfo(sClothes);
+        }
+        
     }
-    function removePara(){
-        $('#eInfo p').remove();
+
+
+    function infoBox(iop, il){
+        $('.infobox').css({
+            opacity: iop,
+            left: il
+        })
     }
+
+    
+  
 })
